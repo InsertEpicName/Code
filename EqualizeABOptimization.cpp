e897ca2ff893245d0430b4f7745ad140def1dfde
InsertEpicName <114185511+InsertEpicName@users.noreply.github.com>
2022-09-26 20:00:34 +0530

    Update EqualizeABOptimization.cpp

--- EqualizeABOptimization.cpp
+++ EqualizeABOptimization.cpp
@@ -1,3 +1,51 @@
+You are given two numbers AA and BB along with an integer XX. In one operation you can do one of the following:
+
+Set A = A + XA=A+X and B = B - XB=B−X
+Set A = A - XA=A−X and B = B + XB=B+X
+Determine if you can make AA and BB equal after applying the operation any number of times (possibly zero).
+
+Input Format
+The first line contains a single integer TT — the number of test cases. Then the test cases follow.
+The first and only line of each test case contains two space-separated integers A, BA,B and XX — the parameters mentioned in the statement.
+Output Format
+For each test case, output YES if you can make AA and BB equal after applying the operation any number of times (possibly zero). Otherwise, output NO.
+
+You can output each letter in any case i.e. YES, yes, yEs are all considered the same.
+
+Constraints
+1 \leq T \leq 10001≤T≤1000
+1 \leq A, B, X \leq 10^91≤A,B,X≤10 
+9
+ 
+Sample 1:
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //My solution 
 #include <iostream>
 using namespace std;
